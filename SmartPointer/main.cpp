504e01d3eb5268eeee0d093186755ff621114fb9
wenmingxing1 <1793701192@qq.com>
2018-01-21 20:32:24 +0800

    增加测试代码

--- SmartPointer/main.cpp
+++ SmartPointer/main.cpp
@@ -1,11 +1,13 @@
 #include "SmartPointer_v1.h"
-
+#include "SmartPointer_v2.h"
 //测试代码
 
+//测试代码中应用的类
 class SomeClass
 {
 public:
     SomeClass() {cout << "SomeClass default constructor" << endl;}
+    ~SomeClass() {cout << "SomeClass destructor" << endl;}
 };
 
 void testcase1()
@@ -17,13 +19,36 @@ void testcase1()
     //创建一个指向类的智能指针
     SmartPointer_v1<SomeClass> spclass = new SomeClass;
     //创建字符串智能指针
-    //SmartPointer_v1<const char> spstr = "Hello World";
+    SmartPointer_v1<const char> spstr = "Hello World";
+}
+
+void testcase2()
+{
+    //
+    SmartPointer_v2<SomeClass> spclass1 = new SomeClass;
+    cout << endl;
+
+    spclass1 = spclass1;    //测试自赋值问题
+    cout << endl;
+
+    //
+    SmartPointer_v2<SomeClass> spclassother(spclass1);
+
+    cout << endl;
+
+    SmartPointer_v2<SomeClass> spclass2 = new SomeClass;
+    cout << endl;
+    cout << "拷贝赋值运算符" << endl;
+    spclass2 = spclass1;    //测试拷贝赋值运算符
+    cout << endl;
 }
 
 int main()
 {
     cout << "***************testcase1：*****************" << endl;
     testcase1();
+    cout << "***************testcase2:******************" << endl;
+    testcase2();
 
     return 0;
 }
