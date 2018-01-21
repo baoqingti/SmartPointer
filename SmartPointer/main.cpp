04f27c975ab6967e8cf2a37b345cf46e15201b55
wenmingxing1 <1793701192@qq.com>
2018-01-21 21:41:47 +0800

    增加测试代码

--- SmartPointer/main.cpp
+++ SmartPointer/main.cpp
@@ -1,13 +1,23 @@
 #include "SmartPointer_v1.h"
 #include "SmartPointer_v2.h"
+#include "SmartPointer_v3.h"
 //测试代码
 
-//测试代码中应用的类
+//测试v1,v2代码中应用的类
 class SomeClass
 {
 public:
     SomeClass() {cout << "SomeClass default constructor" << endl;}
     ~SomeClass() {cout << "SomeClass destructor" << endl;}
+};
+
+//测试v3代码中应用的类
+class SomeClass3: public RefBase
+{
+public:
+    SomeClass3() {cout << "SomeClass3 default constructor" << endl;}
+
+    ~SomeClass3() {cout << "SomeClass3 destructor" << endl;}
 };
 
 void testcase1()
@@ -43,12 +53,39 @@ void testcase2()
     cout << endl;
 }
 
+void testcase3()
+{
+    cout << "观察与testcase2的结果对比,引用计数使得pSomeClass3仅被销毁一次！"
+
+    SomeClass3 *pSomeClass3 = new SomeClass3();
+    cout << pSomeClass3->getRefCount() << endl; //此时输出0
+
+    SmartPointer_v3<SomeClass3> spOuter = pSomeClass3;
+
+    cout << "SomeClass3 Ref Count (" << pSomeClass3->getRefCount() << ") outer 1" << endl;
+
+    {//语句块，可以理解为一个函数，里面所执行的都是形参。
+        SmartPointer_v3<SomeClass3> spInner = spOuter;
+        cout << "SomeClass Ref Count (" << pSomeClass3->getRefCount() << ") inner" << endl;
+    }
+
+    cout << "SomeClass Ref Count (" << pSomeClass3->getRefCount() << ") outer 2" << endl;
+
+    cout << "new another SomeClass3 class for spOuter" << endl;
+    SmartPointer_v3<SomeClass3> spOuter2 = new SomeClass3();
+    spOuter = spOuter2;     //spOuter将会被释放
+
+    //
+}
+
 int main()
 {
     cout << "***************testcase1：*****************" << endl;
     testcase1();
     cout << "***************testcase2:******************" << endl;
     testcase2();
+    cout << "***************testcase3:******************" << endl;
+    testcase3();
 
     return 0;
 }
